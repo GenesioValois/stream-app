a7781413a02e529e2be886a3e60f62b065bbadb4
Genésio Valois <genas96@gmail.com>
2020-09-23 14:05:32 +0100

    Wire up manually create history object

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -7,11 +7,12 @@ import StreamShow from "./streams/StreamShow";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -20,7 +21,7 @@ const App = () => {
           <Route path="/streams/show" exact component={StreamShow} />
           <Route path="/streams/delete" exact component={StreamDelete} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
